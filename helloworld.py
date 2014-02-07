effb6eaa32b3b21de64dda45730ceb525762d708
Glenn Thompson <glennthompson1971@gmail.com>
2014-02-07 07:40:53 -0500

    Adding helloworld

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print "Hello, earthlings!"
+#print "%d" % (2 * 5)
+for i in range(5):
+	print "%d" % (i)
+
+radius = 3
+pi = 3.1415
+circle_area = pi * radius ** 2
+print "A circle of radius %f has area %f" % (radius, circle_area)
+
